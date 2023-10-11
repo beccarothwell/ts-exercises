3ac2fd72012de0efa61791d4477931b6ca3b93bb
Becca Rothwell <becca.rothwell@gmail.com>
2023-10-11 14:18:29 +0100

    feat: Implement exercise 2 - camelCaseWords

--- challenges/exercise2.js
+++ challenges/exercise2.js
@@ -1,26 +1,35 @@
 export function getSquares(nums) {
-	if (nums === undefined) throw new Error('nums is required');
-	// Your code here!
+  if (nums === undefined) throw new Error("nums is required");
+  // Your code here!
 }
 
 export function camelCaseWords(words) {
-	if (words === undefined) throw new Error('words is required');
-	// Your code here!
+  if (words === undefined) throw new Error("words is required");
+  // Your code here!
+
+  const capitalize = words.map((word, i) => {
+    if (i === 0) return word;
+    else if (i > 0) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }
+  });
+
+  return capitalize.join("");
 }
 
 export function getTotalSubjects(people) {
-	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+  if (people === undefined) throw new Error("people is required");
+  // Your code here!
 }
 
 export function checkIngredients(menu, ingredient) {
-	if (menu === undefined) throw new Error('menu is required');
-	if (!ingredient) throw new Error('ingredient is required');
-	// Your code here!
+  if (menu === undefined) throw new Error("menu is required");
+  if (!ingredient) throw new Error("ingredient is required");
+  // Your code here!
 }
 
 export function duplicateNumbers(arr1, arr2) {
-	if (arr1 === undefined) throw new Error('arr1 is required');
-	if (arr2 === undefined) throw new Error('arr2 is required');
-	// Your code here!
+  if (arr1 === undefined) throw new Error("arr1 is required");
+  if (arr2 === undefined) throw new Error("arr2 is required");
+  // Your code here!
 }
