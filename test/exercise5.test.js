6dc1def29fbe8ded35bc18a5b859c9f7a0a45ee6
Becca Rothwell <becca.rothwell@gmail.com>
2023-10-14 23:11:42 +0100

    test: Add decimal tests to exercise 5 - sumMultiples

--- test/exercise5.test.js
+++ test/exercise5.test.js
@@ -19,6 +19,14 @@ describe("sumMultiples", () => {
   test("does not add strings of numbers which are a multiple of 3 or 5", () => {
     expect(sumMultiples([1, 5, 3, "50", "33", "22"])).toBe(8);
   });
+
+  test("works with whole numbers expressed with decimal notation", () => {
+    expect(sumMultiples([1, 5.0, 3.0, 10])).toBe(18);
+  });
+
+  test("does not count fractional numbers expressed with decimal notation", () => {
+    expect(sumMultiples([1, 5.5, 3.33, 10, 5.0])).toBe(15);
+  });
 });
 
 describe("isValidDNA", () => {});
