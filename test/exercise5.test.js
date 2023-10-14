8965500ee83f3b7ad96f5114fbf679e21185808f
Becca Rothwell <becca.rothwell@gmail.com>
2023-10-14 21:21:25 +0100

    test: Correct exercise 5 test - sumMultiples

--- test/exercise5.test.js
+++ test/exercise5.test.js
@@ -9,7 +9,7 @@ import {
 
 describe("sumMultiples", () => {
   test("returns the the sum of any numbers which are a multiple of 3 or 5", () => {
-    expect(findNextNumber([1, 5, 3])).toBe(8);
+    expect(sumMultiples([1, 5, 3])).toBe(8);
   });
 });
 
