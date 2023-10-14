6d57d39145572f215d117ae5a7355102a63acdf2
Becca Rothwell <becca.rothwell@gmail.com>
2023-10-14 21:14:05 +0100

    test: Add test to exercise 5 - sumMultiples

--- test/exercise5.test.js
+++ test/exercise5.test.js
@@ -6,3 +6,19 @@ import {
   createMatrix,
   areWeCovered,
 } from "../challenges/exercise5";
+
+describe("sumMultiples", () => {
+  test("returns the the sum of any numbers which are a multiple of 3 or 5", () => {
+    expect(findNextNumber([1, 5, 3])).toBe(8);
+  });
+});
+
+describe("isValidDNA", () => {});
+
+describe("getComplementaryDNA", () => {});
+
+describe("isItPrime", () => {});
+
+describe("createMatrix", () => {});
+
+describe("areWeCovered", () => {});
