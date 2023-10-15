333c41e1426d966a602174d29b378ad78bb06e97
Becca Rothwell <becca.rothwell@gmail.com>
2023-10-15 21:54:26 +0100

    test: Add exercise6.test.js

---
+++ test/exercise6.test.js
@@ -0,0 +1,17 @@
+import {
+  sumDigits,
+  createRange,
+  getScreentimeAlertList,
+  hexToRGB,
+  findWinner,
+} from "../challenges/exercise5";
+
+describe("sumDigits", () => {});
+
+describe("createRange", () => {});
+
+describe("getScreentimeAlertList", () => {});
+
+describe("hexToRGB", () => {});
+
+describe("findWinner", () => {});
