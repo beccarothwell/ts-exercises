6148b7ecd52d13d418e629f1e9b55950245d94d6
Becca Rothwell <becca.rothwell@gmail.com>
2023-10-15 21:57:30 +0100

    test: Fix test description in exercise 5 - sumMultiples

--- test/exercise5.test.js
+++ test/exercise5.test.js
@@ -8,7 +8,7 @@ import {
 } from "../challenges/exercise5";
 
 describe("sumMultiples", () => {
-  test("returns the the sum of any numbers which are a multiple of 3 or 5", () => {
+  test("returns the sum of any numbers which are a multiple of 3 or 5", () => {
     expect(sumMultiples([5, 1, 3])).toBe(8);
 
     expect(sumMultiples([1, 5, 12, 100, 50, 22, 33, 3])).toBe(203);
